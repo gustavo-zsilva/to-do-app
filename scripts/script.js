4a3b80a2fcf9e9ea5cffbd9f2837bf269a685878
Gustavo Zonta da Silva <gustavozontadasilva@gmail.com>
2020-07-26 20:00:49 -0300

    base project finished

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,107 @@
-const todo = document.querySelector('.new-todo input')
-const addButton = document.querySelector('.add-link')
-const checkbox = document.querySelector('.all-todos ul input')
-const checkboxes = []
-const ul = document.querySelector('ul')
+const input = document.querySelector('.new-todo input')
+const addBtn = document.querySelector('.new-todo .add-link')
+const clear = document.querySelector('.all-todos .clear')
 
-addButton.addEventListener('click', addTodo)
+const spanEl = document.querySelector('span')
 
-function addTodo() {
+const rm = document.querySelector('li a')
+const initialLi = document.querySelector('li')
+
+let ul = document.querySelector('.all-todos ul')
+let allInputs = document.querySelectorAll('input[type=checkbox]')
+
+const todos = []
+
+// var todos = localStorage.getItem('list_todos');
+
+// for (let i = 0; i <= localStorage.length; i++) {
+//     localStorage.getItem(localStorage[i])
+// }
+
+
+
+// Configurações para os to-dos que já carregam na página
+rm.addEventListener('click', () => {
+    initialLi.classList.add('hide')
+
+})
+
+allInputs.forEach(checkbox => {
+    checkbox.addEventListener('click', () => {
+        spanEl.classList.toggle('checked')
+    })
+})
+
+
+addBtn.addEventListener('click', () => {
     
-    let li = document.createElement('li')
-    let a = document.createElement('a')
-    let input = document.createElement('input')
-    input.setAttribute('type', 'checkbox')
-    a.innerHTML += 'X'
-    
-    li.appendChild(a)
-    li.appendChild(input)
-    ul.appendChild(li)
-    li.innerHTML += todo.value
-    
+    if (input.value.length <= 0) {
+        alert('Digite um to-do válido.')
 
-    todo.value = ''
+    } else {
 
-}
+        // Criar as novas variáveis
+        let li = document.createElement('li')
+        let a = document.createElement('a')
+        let newInput = document.createElement('input')
+        let span = document.createElement('span')
+
+        // Setar propriedades das variáveis
+        a.href = '#'
+        a.textContent = 'X'
+        newInput.type = 'checkbox'
+
+        // Adicionar Event Listeners
+        newInput.addEventListener('click', () => {
+            span.classList.toggle('checked')
+        })
+
+        a.addEventListener('click', () => {
+            li.parentNode.removeChild(li)
+        })
+      
+
+        // Adicionar os componentes á tela
+        li.appendChild(a)
+        li.appendChild(newInput)
+        span.textContent = input.value
+        todos.push(span.textContent)
+        li.appendChild(span)
+
+        ul.appendChild(li)
+
+        // saveToStorage()
+
+        // Resetar o campo do input
+        input.value = ''
+        input.focus()
+    }
+
+})
+
+// Limpa todos to-dos
+clear.addEventListener('click', () => {
+    let lis = document.querySelectorAll('li')
+
+    lis.forEach(li => {
+        li.parentNode.removeChild(li)
+    })
+})
+
+// Salvar em localStorage
+// function saveToStorage() {
+
+//     localStorage.setItem(JSON.stringify(index), JSON.stringify(todos[index]))
+
+// }
+
+// localStorage.setItem("name", "domenic")
+// localStorage.clear()
+// localStorage.removeItem("name")
+// localStorage.setItem("age", "30")
+// localStorage.setItem("name", "domenic")
+// console.log(localStorage.key(0));
+
 
 
 
